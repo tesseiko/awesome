24644e3701499056b3ce2dd091a488858d1aeb68
lefmanos <lefmanos@protonmail.com>
2025-02-05 00:03:47 +0200

    add reaper launcher

--- rc.lua
+++ rc.lua
@@ -101,6 +101,7 @@ myawesomemenu = {
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
                                     { "open terminal", terminal },
                                     { "open vlc", "vlc" },
+                                    { "open reaper", "reaper" },
                                     { "open web browser", webbrowser },
                                     { "open file browser", "spacefm" }
                                   }
